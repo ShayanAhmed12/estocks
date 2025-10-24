d441561dd07874b1d4ba1de9a0bdd815ac0bb880
Mohid Raheel <mohid.raheel12@gmail.com>
2025-10-24 22:18:41 +0500

    funds but little work left

--- Controllers/FundsController.cs
+++ Controllers/FundsController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Data;
-using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
+    [AllowAnonymous] // Everyone can access this controller
     public class FundsController : Controller
     {
         private readonly EstocksDbContext _context;
@@ -14,50 +15,27 @@ namespace WebApplication2.Controllers
             _context = context;
         }
 
-        // GET: /Funds
+        // GET: /Funds - Shows list of all funds
         public async Task<IActionResult> Index()
         {
-            var list = await _context.Funds.ToListAsync();
-            return Json(list);
+            var funds = await _context.Funds
+                .OrderBy(f => f.FundName)
+                .ToListAsync();
+
+            return View(funds);
         }
 
-        // GET: /Funds/Details/5
-        public async Task<IActionResult> Details(int id)
+        // GET: /Funds/Details/5 - Shows details of a specific fund
+        public async Task<IActionResult> Details(int? id)
         {
-            var fund = await _context.Funds.FindAsync(id);
+            if (id == null) return NotFound();
+
+            var fund = await _context.Funds
+                .FirstOrDefaultAsync(f => f.FundId == id);
+
             if (fund == null) return NotFound();
-            return Json(fund);
-        }
 
-        // POST: /Funds/Create
-        [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Fund fund)
-        {
-            if (fund == null) return BadRequest();
-            _context.Funds.Add(fund);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(Details), new { id = fund.FundId }, fund);
-        }
-
-        // POST: /Funds/Edit/5
-        [HttpPost]
-        public async Task<IActionResult> Edit(int id, [FromBody] Fund fund)
-        {
-            if (fund == null || id != fund.FundId) return BadRequest();
-            _context.Entry(fund).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-            return NoContent();
-        }
-
-        // POST: /Funds/Delete/5
-        [HttpPost]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var fund = await _context.Funds.FindAsync(id);
-            if (fund == null) return NotFound();
-            _context.Funds.Remove(fund);
-            await _context.SaveChangesAsync();
-            return NoContent();
+            return View(fund);
         }
     }
 }
---
+++ Views/Funds/index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Views.Funds
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
